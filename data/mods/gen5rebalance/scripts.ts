6b9e842b9fae620a982d25f5d12a13dcc7aac65f
SageMcL <125932619+SageMcL@users.noreply.github.com>
2023-09-19 14:57:11 -0500

    Moveset bugfix attempt 2

--- data/mods/gen5rebalance/scripts.ts
+++ data/mods/gen5rebalance/scripts.ts
@@ -1,11 +1,4 @@
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen6',
-	init() {
-		this.modData('Learnsets', 'tyranitar').learnset.sandblast = ['5M'];
-		this.modData('Learnsets', 'hippowdon').learnset.sandblast = ['5M'];
-		this.modData('Learnsets', 'ninetales').learnset.eruption = ['5M'];
-		this.modData('Learnsets', 'arcanine').learnset.eruption = ['5M'];
-		this.modData('Learnsets', 'houndoom').learnset.eruption = ['5M'];
-	},
 	gen: 5,
 };
